63ee4d954309cbd17b7bbdcbaac76c3c64ba1486
uma <info@bhartilodhi.com>
2020-12-17 16:57:20 -0800

    js beautify

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var outputDiv = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/quenya.json";
 
-function getTranslationURL(input)
-{
+function getTranslationURL(input) {
    return serverURL + "?" + "text=" + input;
 }
 
@@ -13,15 +12,14 @@ function errorHandler(error) {
    alert("Server is not responding currently! Try again later.");
 }
 
-function clickHandler()
-{
-var inputText = txtInput.value;
-fetch (getTranslationURL(inputText))
-.then (response=> response.json())
-.then (json =>{
-   var translatedText = json.contents.translated;
-   outputDiv.innerText = translatedText;
-})
-.catch(errorHandler)
+function clickHandler() {
+   var inputText = txtInput.value;
+   fetch(getTranslationURL(inputText))
+      .then(response => response.json())
+      .then(json => {
+         var translatedText = json.contents.translated;
+         outputDiv.innerText = translatedText;
+      })
+      .catch(errorHandler)
 };
-btnTranslate.addEventListener("click",clickHandler);
+btnTranslate.addEventListener("click", clickHandler);
